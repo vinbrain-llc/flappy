67897d77614e2770dc836374b6dd871568f6bbdc
Nguyen Viet My <zzvietmyzz@gmail.com>
2021-04-27 16:00:17 +0700

    Add better feedback

--- lib/flappy.dart
+++ lib/flappy.dart
@@ -1,17 +1,43 @@
 library flappy;
 
-import 'package:flappy/shake_listener.dart';
+import 'package:feedback/feedback.dart';
+import 'package:flappy/shake_detector.dart';
 import 'package:flutter/material.dart';
 
-class FlappyFeedback extends StatelessWidget {
+class FlappyFeedback extends StatefulWidget {
   final Widget child;
   final Function(BuildContext context) listener;
 
   FlappyFeedback({required this.listener, required this.child});
+
+  @override
+  _FlappyFeedbackState createState() => _FlappyFeedbackState((context) => {
+        BetterFeedback.of(context)!.show((feedback, feedbackScreenshot) {
+          alertFeedbackFunction(
+            context,
+            feedback,
+            feedbackScreenshot,
+          );
+        })
+      });
+}
+
+class _FlappyFeedbackState extends State<FlappyFeedback> {
+  final Function(BuildContext) _listener;
+  late ShakeDetector _shakeDetector;
+
+  _FlappyFeedbackState(this._listener) {
+    _shakeDetector = ShakeDetector(() => _listener.call(context));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: ShakeListener(listener: listener, child: child),
-    );
+    return BetterFeedback(child: widget.child);
+  }
+
+  @override
+  void dispose() {
+    _shakeDetector.stop();
+    super.dispose();
   }
 }
