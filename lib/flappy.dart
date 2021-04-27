aabd12653ad431073b3aa9e03f3a0aae8e547224
Nguyen Viet My <zzvietmyzz@gmail.com>
2021-04-27 15:27:37 +0700

    Set up init

--- lib/flappy.dart
+++ lib/flappy.dart
@@ -4,11 +4,14 @@ import 'package:flappy/shake_listener.dart';
 import 'package:flutter/material.dart';
 
 class FlappyFeedback extends StatelessWidget {
+  final Widget child;
+  final Function(BuildContext context) listener;
+
+  FlappyFeedback({this.listener, this.child});
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: ShakeListener(
-          listener: (context) => print("alo"), child: Container()),
+      child: ShakeListener(listener: listener, child: child),
     );
   }
 }
